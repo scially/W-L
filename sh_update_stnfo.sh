108089cf4cd35bc00fcf35b3bd6aade17e84358d
WangSmallHui <wzxwhd@126.com>
2017-04-27 19:38:40 +0800

    sh_upd_stnfo BUG Fix

---
+++ sh_update_stnfo.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+#author:wanghui
+#email:wzxwhd@126.com
+#introduce
+if [ $# -eq 0 ];then
+ echo "update the rinex files to station.info"
+ echo "firstly,you need to ready a station.info model"
+ echo "Looking forward that you give me a new method for this shell"
+ echo "./usage sh_update_stnfo.sh -yr <year>"
+ exit
+fi
+echo "sh_update_stnfo version 2017/4/27"
+echo "Input Option:$@"
+year=""
+while [ $# -gt 0 ]
+do
+ case $1 in
+  "-yr")
+  year=$2
+  ;;
+ esac
+ shift
+done
+echo ${year:0-2:2}
+for file in `ls`
+do
+	if [[ $file =~ [a-zA-Z0-9]{8}.${year:0-2:2}o ]];then
+	echo "update $file"
+	sh_upd_stnfo -files $file >> updation.log
+	fi
+done
