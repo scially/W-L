31fbb43ae0197a362e65f076d7a2eba5aaad6cd6
WangSmallHui <wzxwhd@126.com>
2017-10-25 10:15:48 +0800

    更新路径问题

--- GamitShell/sh_update_tables.py
+++ GamitShell/sh_update_tables.py
@@ -8,6 +8,7 @@ import ftplib
 import sys
 import os
 
+
 def connect(host):
     try:
         ftp = ftplib.FTP(host, user='anonymous', passwd='anonymous')
@@ -15,7 +16,7 @@ def connect(host):
         ftp.login()
         return ftp
     except:
-        print 'FTP is unavailable, pleace check you internet'
+        print('FTP is unavailable, pleace check you internet')
         sys.exit(0)
 
 
@@ -29,21 +30,21 @@ def LnkTables(tables):
         if os.path.exists(os.path.join(Out, key)) and val != None:
             cps = 'sudo cp -f {0} {1}'.format(os.path.join(Out, key), os.path.join(Path, 'tables', key))
             lnk = 'sudo ln -sf {0} {1}'.format(os.path.join(Path, 'tables', key), os.path.join(Path, 'tables', val))
-            print cps
+            print(cps)
             os.system(cps)
-            print lnk
+            print(lnk)
             os.system(lnk)
 
 if len(sys.argv) == 1:
     print(
         '''
         Updating tables file in Gamit10.x by this py-shell
-        Usage: ./sh_update_tables.py -link n -grid <y/n> -version <v>
+        Usage: ./sh_update_tables.py -link n -grid <y/n> -version <v> -path <p>
                 -year <year> -out <path>
             -link    link the downloading files in tables, this need to root
             -grid    download grids [default is n]
-            -version the version of gamit, fomat of some tables file is different between
-                     under 10.6 and 10.6 [default is 10.6]
+            -path    the path of gamit[default is /opt/gamit10.6]
+            -version the version of gamit[default is 10.6]
             -out     on which download tables [default is current]
         Example: ./sh_update_tables.py -link y -grid n -version 10.6 -year 2017 
         ''')
@@ -53,11 +54,8 @@ Grid = False
 Version = 10.6
 year = 2017
 Out = os.path.join(os.getcwd(),'tables')
-Path=os.getenv('HELP_DIR')
-if Path== None:
-    print 'Gamit has some errors!'
-    sys.exit()
-Path=os.path.dirname(os.path.dirname(Path))
+Path='/opt/gamit10.6'
+
 for index in range(0, len(sys.argv)):
     if sys.argv[index] == '-link':
         Link = True if sys.argv[index + 1] == 'y' else False
@@ -69,6 +67,8 @@ for index in range(0, len(sys.argv)):
         year = sys.argv[index + 1]
     elif sys.argv[index] == '-out':
         Out = sys.argv[index + 1]
+    elif sys.argv[index]=='-path':
+        Path = sys.argv[index + 1]
 tables = {'pole.usno': 'pole.',
           'ut1.usno': 'ut1.',
           'luntab.{0}.J2000'.format(year): 'luntab.',
@@ -89,27 +89,27 @@ grids = {'vmf1grd.{0}'.format(year): 'map.grid.{0}'.format(year),
 
 # download tables
 # ftp://garner.ucsd.edu/
-print 'Updating tables'
+print('Updating tables')
 if (Out == None or not os.path.exists(Out)):
-    print 'path not exist, we will create this path: %s' % Out
+    print('path not exist, we will create this path: %s' % Out)
     os.mkdir(Out)
 ftp = connect('132.239.152.183')
 ftp.cwd('/pub/gamit/tables')
 files = ftp.nlst()
 for key, val in tables.items():
-    print 'updating {0} to {1}'.format(key, Out)
+    print('updating {0} to {1}'.format(key, Out))
     download(ftp, Out, key)
 ftp.quit()
 
 # download grids
 # ftp://everest.mit.edu
 if Grid:
-    print 'Updating Grids'
+    print('Updating Grids')
     ftp = connect('18.83.0.21')
     ftp.cwd('/pub/GRIDS')
     files = ftp.nlst()
     for key, val in grids.items():
-        print 'updating {0} to {1}'.format(key, Out)
+        print('updating {0} to {1}'.format(key, Out))
         download(ftp, Out, key, 5 * 1024)
     ftp.quit()
 
