d6d71622605558a488a437683f2cb8e12e443e61
WangHui <wzxwhd@126.com>
2018-01-03 18:26:15 +0800

    add progressbar and fixbugs

--- GamitShell/sh_update_tables.py
+++ GamitShell/sh_update_tables.py
@@ -1,74 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- encoding:utf-8 -*-
 """
-Updating tables file in Gasmit10.x by this py-shell
-更改链接规则
+Updating tables file in Gasmit10.x by this pyhon script
+authors: wanghui
+emails : wzxwhd@126.com
 """
 import ftplib
 import sys
 import os
-
-
-def connect(host):
-    try:
-        ftp = ftplib.FTP(host, user='anonymous', passwd='anonymous')
-        ftp.connect()
-        ftp.login()
-        return ftp
-    except:
-        print('FTP is unavailable, pleace check you internet')
-        sys.exit(0)
-
-
-def download(ftp, out, filename, blocksize=2048):
-    if key in files:
-        with open(os.path.join(out, filename), 'wb') as f:
-            ftp.retrbinary('RETR ' + key, f.write, blocksize=blocksize)
-
-def LnkTables(tables):
-    for key, val in tables.items():
-        if os.path.exists(os.path.join(Out, key)) and val != None:
-            cps = 'sudo cp -f {0} {1}'.format(os.path.join(Out, key), os.path.join(Path, 'tables', key))
-            lnk = 'sudo ln -sf {0} {1}'.format(os.path.join(Path, 'tables', key), os.path.join(Path, 'tables', val))
-            print(cps)
-            os.system(cps)
-            print(lnk)
-            os.system(lnk)
-
-if len(sys.argv) == 1:
-    print(
-        '''
-        Updating tables file in Gamit10.x by this py-shell
-        Usage: ./sh_update_tables.py -link n -grid <y/n> -version <v> -path <p>
-                -year <year> -out <path>
-            -link    link the downloading files in tables, this need to root
-            -grid    download grids [default is n]
-            -path    the path of gamit[default is /opt/gamit10.6]
-            -version the version of gamit[default is 10.6]
-            -out     on which download tables [default is current]
-        Example: ./sh_update_tables.py -link y -grid n -version 10.6 -year 2017 
-        ''')
-    sys.exit()
+import functools
+# global configure
+# modify these parametes according to your system
 Link=False
 Grid = False
 Version = 10.6
-year = 2017
+year = 2018
 Out = os.path.join(os.getcwd(),'tables')
-Path='/opt/gamit10.6'
-
-for index in range(0, len(sys.argv)):
-    if sys.argv[index] == '-link':
-        Link = True if sys.argv[index + 1] == 'y' else False
-    elif sys.argv[index] == '-grid':
-        Grid = True if sys.argv[index + 1] == 'y' else False
-    elif sys.argv[index] == '-version':
-        Version = float(sys.argv[index + 1])
-    elif sys.argv[index] == '-year':
-        year = sys.argv[index + 1]
-    elif sys.argv[index] == '-out':
-        Out = sys.argv[index + 1]
-    elif sys.argv[index]=='-path':
-        Path = sys.argv[index + 1]
+Path='~/gamit10.6'
 tables = {'pole.usno': 'pole.',
           'ut1.usno': 'ut1.',
           'luntab.{0}.J2000'.format(year): 'luntab.',
@@ -84,14 +32,91 @@ tables = {'pole.usno': 'pole.',
           'dcb.dat.gps': 'dcb.dat',
           }
 grids = {'vmf1grd.{0}'.format(year): 'map.grid.{0}'.format(year),
-         'atmdisp_cm.{0}'.format(year): 'atml.grid.{0}'.format(year),
-         }
+        'atmdisp_cm.{0}'.format(year): 'atml.grid.{0}'.format(year),
+        }
+__current_size = 0
+
+# construct ftp object
+def connect(host):
+    try:
+        ftp = ftplib.FTP(host, user='anonymous', passwd='anonymous')
+        ftp.connect()
+        ftp.login()
+        return ftp
+    except:
+        print('FTP is unavailable, please check you system')
+        sys.exit(1)
+
+def write_callback(totalsize, fileobjct, blocks):
+    global __current_size
+    __current_size += len(blocks)
+    fileobjct.write(blocks)
+    #print('总大小{}，当前大小{}, 百分比{}'.format(totalsize, __current_size, __current_size/totalsize*100))
+    progressbar(__current_size, totalsize)
+
+def download(ftp, out, filename, blocksize=20):   
+    global __current_size    
+    if key in files:
+        filesize = ftp.size(filename)
+        with open(os.path.join(out, filename), 'wb') as f:   
+            callback = functools.partial(write_callback,filesize, f)
+            ftp.retrbinary('RETR ' + filename, callback, blocksize=blocksize)
+        print('\n')
+        __current_size = 0
+
+def LnkTables(tables):
+    for key, val in tables.items():
+        if os.path.exists(os.path.join(Out, key)) and val != None:
+            cps = 'cp -f {0} {1}'.format(os.path.join(Out, key), os.path.join(Path, 'tables', key))
+            lnk = 'ln -sf {0} {1}'.format(os.path.join(Path, 'tables', key), os.path.join(Path, 'tables', val))
+            print(cps)
+            os.system(cps)
+            print(lnk)
+            os.system(lnk)
+
+def progressbar(cur, total, width=50):
+    percent = cur / total
+    sys.stdout.write(' ' * (width + 10) + '\r')
+    sys.stdout.write('\r')
+    sys.stdout.write('{:3.0%}[{}]'.format(percent, 
+                    '#'*int(width*percent) + '-'*(width - int(width*percent))))
+    sys.stdout.flush()
+    
+if len(sys.argv) == 1:
+    print(
+        '''
+        Updating tables file in Gamit10.x by this py-shell
+        Usage: [sudo] python3 sh_update_tables.py -link n -grid <y/n> -version <v> -path <p>
+                -year <year> -out <path>
+            -link    link the downloading files in tables, this need to root[y/n]
+            -grid    download grids [default is n]
+            -path    the path of gamit[default is ~/gamit10.6]
+            -version the version of gamit[default is 10.6]
+            -out     on which download tables [default is current path]
+        Example: [sudo] python3 sh_update_tables.py -link y -grid n -version 10.6 -year 2017 
+        ''')
+    sys.exit(1)
+
+for index in range(0, len(sys.argv)):
+    if sys.argv[index] == '-link':
+        Link = True if sys.argv[index + 1] == 'y' else False
+    elif sys.argv[index] == '-grid':
+        Grid = True if sys.argv[index + 1] == 'y' else False
+    elif sys.argv[index] == '-version':
+        Version = float(sys.argv[index + 1])
+    elif sys.argv[index] == '-year':
+        year = sys.argv[index + 1]
+    elif sys.argv[index] == '-out':
+        Out = sys.argv[index + 1]
+    elif sys.argv[index]=='-path':
+        Path = sys.argv[index + 1]
 
 # download tables
 # ftp://garner.ucsd.edu/
 print('Updating tables')
 if (Out == None or not os.path.exists(Out)):
-    print('path not exist, we will create this path: %s' % Out)
+    print('path not exist, we will create this path: {0}'
+          .format(Out))
     os.mkdir(Out)
 ftp = connect('132.239.152.183')
 ftp.cwd('/pub/gamit/tables')
